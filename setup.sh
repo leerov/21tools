8fc492d57010266b628dbb8134a1d4cd06626615
Владимир <ZhmulevVV@gmail.com>
2025-03-15 14:50:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
-ln -s ~/21tools/zshrc ~/.zshrc
+#!/bin/bash
+if [ ! -f ~/.zshrc ]; then
+    touch ~/.zshrc
+fi
+if ! grep -q "source ~/21tools/zshrc" ~/.zshrc; then
+    echo "source ~/21tools/zshrc" >> ~/.zshrc
+fi
